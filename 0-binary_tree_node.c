0aab0ed8be5e3314e52f326050bf55a3c3f6139b
MrHelsinki <taha.chad2001@gmail.com>
2024-03-28 05:26:12 +0000

    tests

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,15 +1,15 @@
-#include "main.h"
+#include "binary_trees.h"
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     
-    binary_tree_t *tmp = malloc(sizeof(binary_tree_t));
+    binary_tree_t *tmp = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
     if (tmp = NULL)
     {
         return (NULL);
     }
-    
+
     tmp->parent = parent;
     tmp->n = value;
     tmp->left = NULL;
