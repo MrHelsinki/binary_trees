b899e44d4369ee2c533aaa512c7610bfe1c3259c
MrHelsinki <taha.chad2001@gmail.com>
2024-03-28 06:15:38 +0000

    commit auto

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,20 @@
-#include <stdio.h>
+#include "binary_trees.h"
 
-int main(void)
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    printf("Hello, World!");
-    return (0);
+    binary_tree_t *node;
+
+    node = malloc(sizeof(binary_tree_t));
+    if (node == NULL)
+    {
+        return (NULL);
+    }
+
+    node->parent = parent;
+    node->left = NULL;
+    node->right = NULL;
+    node->n = value;
+    return (node);
 }
+
