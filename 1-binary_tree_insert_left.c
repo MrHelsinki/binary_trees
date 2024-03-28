a283469aa4b0d456025691891b1b8cac07353157
MrHelsinki <taha.chad2001@gmail.com>
2024-03-28 06:55:14 +0000

    commit auto

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,11 +13,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL || parent == NULL)
-	{
+	if (parent == NULL)
 		return (NULL);
-	}
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+
 	node->parent = parent;
 	node->right = NULL;
 	node->n = value;
