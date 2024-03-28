ef780fa3243a2e1917550974c4ba0cde09161775
MrHelsinki <taha.chad2001@gmail.com>
2024-03-28 06:29:14 +0000

    commit auto

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,20 +1,27 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - create node
+ *
+ * @parent: parent node
+ * @value: value of node
+ *
+ * Return: created node ptr
+*/
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node;
+	binary_tree_t *node;
 
-    node = malloc(sizeof(binary_tree_t));
-    if (node == NULL)
-    {
-        return (NULL);
-    }
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+	{
+		return (NULL);
+	}
 
-    node->parent = parent;
-    node->left = NULL;
-    node->right = NULL;
-    node->n = value;
-    return (node);
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	node->n = value;
+	return (node);
 }
-
